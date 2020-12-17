1407f5df7d2c30f89801c3adb99bfaeedf796f62
Kerry Snyder <kerry@atmanaut.me>
2020-12-16 19:29:52 -0800

    friend-sized site title

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,8 +4,8 @@ import SiteNav from './site-nav'
 const Intro = ({pages}) => {
   return (
     <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
-      <h1 className="text-6xl md:text-8xl md:pr-8 lg:w-1/3 pretty-heading">{SITE_TITLE}.</h1>
-      <SiteNav classes="mt-5 text-xl text-center md:pl-8 lg:w-2/3 md:text-right" pages={pages} />
+      <h1 className="text-6xl md:text-7xl lg:text-8xl md:pr-8 lg:w-1/2 pretty-heading">{SITE_TITLE}.</h1>
+      <SiteNav classes="mt-5 text-xl text-center md:pl-8 lg:w-1/2 md:text-right" pages={pages} />
     </section>
   )
 }
