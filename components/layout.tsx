b4ec1e841f603b6872d1255b83803121fdca86ef
Kerry Snyder <kerry@atmanaut.me>
2021-01-05 22:01:21 -0800

    rename mode switch to theme switch

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,13 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
-import ModeSwitch from './mode-switch'
+import ThemeSwitch from './theme-switch'
 
 const Layout = ({preview, children}) => {
   return (
     <>
       <Meta />
-      <ModeSwitch />
+      <ThemeSwitch />
       <div className="min-h-screen">
         <Alert preview={preview} />
         <main>{children}</main>
