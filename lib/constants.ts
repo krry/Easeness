7106094d1331746a2756936707217bdeda76e190
Kerry Snyder <kerry@atmanaut.me>
2021-01-17 14:57:33 -0800

    light og image

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,5 +4,5 @@ export const SITE_TITLE = 'Easeness'
 export const GITHUB_ORG = 'Atmanautica'
 export const CMS_URL = 'https://atmanautica.sanity.studio/'
 export const HOME_OG_IMAGE_URL =
-  'https://og-image.atmanaut.us/Easeness.jpeg?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fatmanaut.sirv.com%2F_images%2Fbloob-white.png&widths=350&heights=350'
+  'https://og-image.atmanaut.us/Easeness.jpeg?theme=light&md=1&fontSize=125px&images=https%3A%2F%2Fatmanaut.sirv.com%2F_images%2Fbloob-black.png&widths=350&heights=350'
 export const SITE_DESCRIPTION = 'Easeness brings ease to busyness to make business easy.'
