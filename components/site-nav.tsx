cdcaf6ab95fd308363606bdc4969dfa829162ef9
Kerry Snyder <kerry@atmanaut.me>
2021-08-13 17:04:13 -0700

    👌 IMPROVE: alphabetize site nav links

--- components/site-nav.tsx
+++ components/site-nav.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link'
 
+function alphabetize(a, b) {
+	const nameA = a.link.toUpperCase() // ignore upper and lowercase
+	const nameB = b.link.toUpperCase() // ignore upper and lowercase
+	if (nameA < nameB) {
+		return -1
+	}
+	if (nameA > nameB) {
+		return 1
+	}
+
+	// names must be equal
+	return 0
+}
+
 const SiteNav = ({ pages, classes }) => {
 	return (
 		pages?.length > 0 && (
 			<nav className={classes}>
 				<ul>
-					{pages.map(page => (
+					{pages.sort(alphabetize).map(page => (
 						<li
 							key={page.slug}
 							className='inline-block px-2 font-semibold md:mx-2 '>
